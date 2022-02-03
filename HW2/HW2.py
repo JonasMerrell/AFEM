dc08515cf8788b37b17314cb05caffd67a5c5235
Corellian101 <jonas@DESKTOP-I495EGR.localdomain>
2022-02-02 17:06:47 -0700

    formating

--- HW2/HW2.py
+++ HW2/HW2.py
@@ -1,5 +1,6 @@
 import numpy as np
 from Solve import solve
+from sympy import print_latex, Matrix
 
 coords = np.array([[0,0,0], [10,5,0], [10,0,0], [20,8,0], [20,0,0],
                    [30,9,0], [30,0,0], [40,8,0], [40,0,0], [50,5,0],
@@ -9,15 +10,13 @@ connect = np.array([[1,3], [3,5], [5,7], [7,9], [9,11], [11,12],
                     [1,2], [2,4], [4,6], [6,8], [8,10], [10,12],
                     [2,3], [4,5], [6,7], [8,9], [10,11], [2,5],
                     [4,7], [7,8], [9,10]])-1
-
 youngs_modulus = np.array([1000]*21)
 Abot, Atop, Abat, Adia = 2, 10, 3, 1
 
 area = np.array([Abot, Abot, Abot, Abot, Abot, Abot,
-     Atop, Atop, Atop, Atop, Atop, Atop,
-     Abat, Abat, Abat, Abat, Abat,
-     Adia, Adia, Adia, Adia])
-
+                 Atop, Atop, Atop, Atop, Atop, Atop,
+                 Abat, Abat, Abat, Abat, Abat,
+                 Adia, Adia, Adia, Adia])
 num_nodes = coords.shape[0]
 
 X, Y, Z = (0, 1, 2)
@@ -27,7 +26,10 @@ loads = [[2,Y,-10.],[4,Y,-10.],[6,Y,-16.],[8,Y,-10.],[10,Y,-10.]]
 bcs = [[0, ALL_dofs, 0.], [11, Y, 0.], [ALL_nodes, Z, 0]]
 
 u, R = solve(coords, connect, area, youngs_modulus, loads, bcs)
-
+u = np.reshape(u, (-1,3))
+R = np.reshape(np.round(R), (-1,3))
+print_latex(Matrix(R))
+print_latex(Matrix(np.round(u,4)))
 # from element import stiffness
 # from global_stiffness import assemble
 # from global_force import format_bcs, global_force, apply_bcs
--- HW2/Solve.py
+++ HW2/Solve.py
@@ -1,11 +1,11 @@
 import numpy as np
 from global_stiffness import assemble
-from global_force import global_force, assemble_force, format_bcs
+from global_force import global_force, apply_bcs, format_bcs
 
 def solve(coords, connect, area, youngs_modulus, loads, bcs):
     K = assemble(coords, connect, area, youngs_modulus)
-    Kbc, Fbc = assemble_force(K, coords, loads, bcs)
     F = global_force(*format_bcs(loads, bcs, coords))
+    Kbc, Fbc = apply_bcs(K, F, *format_bcs(loads, bcs, coords))
     u = np.linalg.solve(Kbc, Fbc)
     Ft = K @ u
     R = Ft - F
